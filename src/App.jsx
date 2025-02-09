e29f7415f65b8d24f322a122a1e9e3dd636f773f
Dima121880 <surguladze.dima33@gmail.com>
2025-02-10 01:44:34 +0400

    add cars

--- src/App.jsx
+++ src/App.jsx
@@ -3,86 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-// function App() {
-//   const [Firstname, setFirstname] = useState("");
-//   const [lastname, setlastname] = useState("");
-
-//   const clear = () => {
-//     setFirstname("");
-//     setlastname("");
-//   };
-//   return (
-//     <>
-//       <input
-//         type="text"
-//         value={Firstname}
-//         placeholder="Firstname"
-//         onChange={(e) => setFirstname(e.target.value)}
-//       />
-//       <br />
-//       <input
-//         type="text"
-//         value={lastname}
-//         placeholder="lastname"
-//         onChange={(e) => setlastname(e.target.value)}
-//       />
-//       <br />
-//       {Firstname} {lastname}
-//       <br />
-//       <button onClick={() => clear()}>clear</button>
-//       <br />
-//       <div>
-//         <a href="" target="_blank">
-//           <img
-//             src={
-//               "https://tse2.mm.bing.net/th?id=OIP.JDa_1X-NqdM1K-bZxp99DQHaEo&pid=Api&P=0&h=220"
-//             }
-//             className="logo"
-//             alt="Vite logo"
-//           />
-//         </a>
-//         <a href="" target="">
-//           <img
-//             src={
-//               "https://tse4.mm.bing.net/th?id=OIP.HxV79tFMPfBAIo0BBF-sOgHaEy&pid=Api&P=0&h=220"
-//             }
-//             className="logo react"
-//             alt="React logo"
-//           />
-//         </a>
-//         <a href="" target="_blank">
-//           <img
-//             src={
-//               "https://tse3.mm.bing.net/th?id=OIP.rt-pujAJJlfsYntGE4rRAgHaE7&pid=Api&P=0&h=220"
-//             }
-//             className="logo"
-//             alt="Vite logo"
-//           />
-//         </a>
-//         <a href="" target="_blank">
-//           <img
-//             src={
-//               "https://tse3.mm.bing.net/th?id=OIP.0iqvqUM-_MntTZp4CMBaigHaEK&pid=Api&P=0&h=220"
-//             }
-//             className="logo react"
-//             alt="React logo"
-//           />
-//         </a>
-//         <a href="" target="_blank">
-//           <img
-//             src={
-//               "https://tse4.mm.bing.net/th?id=OIP.C2jx84-6xaPAth9E4dkzGAHaHa&pid=Api&P=0&h=220"
-//             }
-//             className="logo"
-//             alt="Vite logo"
-//           />
-//         </a>
-//       </div>
-//     </>
-//   );
-
-// }
-
 function App() {
   const [value, setvalue] = useState();
 
@@ -188,27 +108,6 @@ function App() {
             </li>
           </ul>
         </a>
-        {/* <div
-          className="cls63"
-          style={{
-            border: "1px solid red",
-            width: "600px",
-            height: "200px",
-          }}
-        >
-          <a href="https://www.myauto.ge/ka/pr/112011839/iyideba-manqanebi-sedani-mercedes-benz-cls-450-2019-benzini-tbilisi?offerType=basic&source=search">
-            <img
-              className="merso"
-              style={{
-                width: "120px",
-                height: "100px",
-              }}
-              src="https://static.my.ge/myauto/photos/3/8/1/1/0/large/112011839_1.jpg?v=3"
-              alt="cls63"
-            />
-            <p>2016 Mercedes-Benz CLS 400</p>
-          </a>
-        </div> */}
       </div>
     );
   };
